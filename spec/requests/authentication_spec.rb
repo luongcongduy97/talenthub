6d0dcbb94662cdf9bf578ce494d1c942c7620835
luongcongduy97 <duylcong97@gmail.com>
2025-07-07 16:48:34 +0700

    add test for auth

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Authentication', type: :request do
+  describe 'registration' do
+    it 'allows user to register' do
+      expect do
+        post user_registration_path, params: {
+          user: {
+            email: 'newuser@example.com',
+            password: 'password123',
+            password_confirmation: 'password123'
+          }
+        }
+      end.to change(User, :count).by(1)
+
+      user = User.order(:id).last
+      expect(response).to redirect_to(user_path(user))
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'login' do
+    it 'allows user to login' do
+      user = User.create!(email: 'login@example.com', password: 'password123', password_confirmation: 'password123')
+
+      post user_session_path, params: { user: { email: user.email, password: 'password123' } }
+
+      expect(response).to redirect_to(user_path(user))
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
---
+++ spec/system/authentication_spec.rb
@@ -0,0 +1,33 @@
+
+require 'rails_helper'
+
+RSpec.describe 'Authentication', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'allows a user to register' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'systemuser@example.com'
+    fill_in 'Password', with: 'password123'
+    fill_in 'Password confirmation', with: 'password123'
+    click_button 'Sign up'
+
+    user = User.order(:id).last
+    expect(page).to have_current_path(user_path(user))
+    expect(page).to have_content('User Details')
+  end
+
+  it 'allows a user to log in' do
+    User.create!(email: 'systemlogin@example.com', password: 'password123', password_confirmation: 'password123')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'systemlogin@example.com'
+    fill_in 'Password', with: 'password123'
+    click_button 'Log in'
+
+    user = User.find_by(email: 'systemlogin@example.com')
+    expect(page).to have_current_path(user_path(user))
+    expect(page).to have_content('User Details')
+  end
+end
