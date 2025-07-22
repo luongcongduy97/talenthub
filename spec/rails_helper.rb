79776bb6db0079d934540a44c716914efbeeaa4c
luongcongduy97 <duylcong97@gmail.com>
2025-07-22 12:53:43 +0700

    rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.before(:each, type: :system) do
     driven_by(:rack_test)
   end
