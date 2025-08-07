eaf4622e13bbb49d30a5d200b59fd59f651c344a
luongcongduy97 <duylcong97@gmail.com>
2025-08-07 20:45:41 +0700

    fix spec

--- spec/system/companies_spec.rb
+++ spec/system/companies_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Companies', type: :system do
     fill_in 'Industry', with: 'Tech'
     click_button 'Create Company'
 
-    company = Company.last
+    company = Company.find_by(name: 'Test Corp')
     expect(page).to have_current_path(company_path(company))
     expect(page).to have_content('Test Corp')
 
