dc98a52440c56c7455da3cee102bbf1a55b11f5f
luongcongduy97 <duylcong97@gmail.com>
2025-09-12 20:35:14 +0700

    model search

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,10 @@
 class Employee < ApplicationRecord
   belongs_to :company
   belongs_to :user
+
+  # Full text search on name and position
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_position,
+                  against: %i[name position],
+                  using: { tsearch: { prefix: true } }
 end
