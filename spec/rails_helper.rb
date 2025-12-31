7fc93e6c458bf4a9d0d9a68725fdc7b643cced0d
luongcongduy97 <duylcong97@gmail.com>
2025-12-31 18:10:53 +0700

    fix spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,16 +23,18 @@ RSpec.configure do |config|
   config.include AuthenticationHelper, type: :system
 
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |options|
+    driven_by :selenium, using: :headless_chrome do |options|
+      options.add_argument('--window-size=1400,1400')
+
       options.add_preference('credentials_enable_service', false)
       options.add_preference('profile.password_manager_enabled', false)
-
       options.add_argument('--disable-features=PasswordLeakDetection')
-
 
       options.add_argument('--disable-notifications')
       options.add_argument('--disable-infobars')
       options.add_argument('--disable-gpu')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
     end
   end
 end
