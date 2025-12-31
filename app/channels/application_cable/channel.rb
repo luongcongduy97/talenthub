7c1468f907477ed170440a4e600c6bc7f624ba49
luongcongduy97 <duylcong97@gmail.com>
2025-12-31 15:32:17 +0700

    mark as read 1

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,25 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+    rescue_from StandardError, with: :handle_standard_error
+
+    private
+
+    def handle_not_found(exception)
+      Rails.logger.error "ActionCable 404: #{exception.message}"
+
+      transmit({ error: "Resources not found or deleted." })
+
+      reject
+    end
+
+    def handle_standard_error(exception)
+      Rails.logger.error "ActionCable Error: #{exception.message}"
+      if exception.backtrace
+        Rails.logger.error exception.backtrace.join("\n")
+      end
+
+      transmit({ error: "Something went wrong on the real-time server." })
+    end
   end
 end
--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -7,6 +7,11 @@ class NotificationsChannel < ApplicationCable::Channel
     end
   end
 
+  def mark_as_read(data)
+    notification = current_user.notifications.find(data["id"])
+    notification.update!(read_at: Time.current)
+  end
+
   def unsubscribed
   end
 end
