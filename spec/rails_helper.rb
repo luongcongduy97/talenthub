0e46e03a955aceb9050c0f78023a665eefddb06c
luongcongduy97 <duylcong97@gmail.com>
2025-12-31 18:15:33 +0700

    fix spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   config.include AuthenticationHelper, type: :system
 
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome do |options|
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |options|
       options.add_argument('--window-size=1400,1400')
 
       options.add_preference('credentials_enable_service', false)
